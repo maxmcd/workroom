9bc5ef6631ad98f2bc4b50c4a9d4b0162a80ffbb
maxmcd <max.t.mcdonnell@gmail.com>
2014-11-25 16:48:37 -0500

    fake cursor test

--- public/app.js
+++ public/app.js
@@ -51,6 +51,18 @@ $(function() {
             'remote'
         )
         editor.doc.setHistory(history)
+
+        // var cursor_coords = editor.cursorCoords(changeObj.to)
+        // console.log(cursor_coords)
+        // var fake_cursor = document.createElement('div')
+        // fake_cursor.className = "CodeMirror-cursor"
+        // fake_cursor.innerHTML = "&nbsp;"
+        // fake_cursor.style.top = (cursor_coords.top - 4) + 'px'
+        // fake_cursor.style.left = (cursor_coords.left - 29) + 'px'
+        // fake_cursor.style.height = '13px'
+        // console.log(fake_cursor.style)
+
+        // $('.CodeMirror-cursors').append(fake_cursor)
     })
 
 })
